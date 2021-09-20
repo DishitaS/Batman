d5bbff12980bf557ce664bfb5feedac0f6624609
DishitaS <76867580+DishitaS@users.noreply.github.com>
2021-09-20 15:55:01 +0530

    Add files via upload

---
+++ Drops.js
@@ -0,0 +1,15 @@
+class Drops{
+    constructor(x,y){
+        var options={
+            friction:0.1,
+        }
+        
+        this.rain=Bodies.circle(x,y,5,options)
+    }
+
+    update(){
+        if(this.rain.positionY<height){
+            Matter.Body.setPosition(this.rain,{x:random (0,400), y:random(0,400)});
+        }
+    }
+} 
